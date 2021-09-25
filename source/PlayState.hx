5017d0e00402eb0afe09ea4a469a3fa664da9bba
itaylaptop <itaybn100@gmail.com>
2021-09-25 20:29:39 +0300

    Added Rod to screen

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,18 @@
 package;
 
 import flixel.FlxState;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
+	var rod:Rod;
+
 	override public function create()
 	{
+		bgColor = FlxColor.CYAN;
+		rod = new Rod();
+		rod.screenCenter(X);
+		add(rod);
 		super.create();
 	}
 
